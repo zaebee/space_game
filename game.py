1efaef380d978b614edaf419b453c359569f5648
andrei G <sinezub@yandex.ru>
2024-09-30 20:34:45 +0300

    Fix image render

--- game.py
+++ game.py
@@ -14,18 +14,13 @@ class Game:
         pygame.init()
         self.running = True
         self.clock = pygame.time.Clock()
+        self.bg = pygame.image.load('images/kosmos.png')
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-        pygame.display.set_caption(self.CAPTION)
-        self.init_images()
         self.hero = player.Player(400, 400)
     
     def display_fps(self):
         caption = '{} - FPS: {:.2f}'.format(self.CAPTION, self.clock.get_fps())
         pygame.display.set_caption(caption)
-    
-    def init_images(self):
-        bg_surface = pygame.image.load('images/kosmos.png')
-        self.screen.blit(bg_surface, bg_surface.get_rect())
     
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -33,20 +28,22 @@ class Game:
         self.hero.on_event(event)
         
     def on_render(self):
+        self.screen.fill((0, 0, 0))
+        self.screen.blit(self.bg, self.bg.get_rect())
         self.screen.blit(
-            self.hero.surface, (self.hero.x, self.hero.y))
+            self.hero.image, (self.hero.x, self.hero.y))
+        self.display_fps()
+        self.clock.tick(self.FPS)
+        pygame.display.flip()
     
     def on_cleanup(self):
         pygame.quit()
         
     def on_execute(self):
         while self.running:
-            self.clock.tick(self.FPS)
-            self.display_fps()
+            self.on_render()
             for event in pygame.event.get():
                 self.on_event(event)
-            self.on_render()
-            pygame.display.flip()
         self.on_cleanup()
         
 game = Game()
--- player.py
+++ player.py
@@ -1,24 +1,26 @@
 import pygame
 
 class Player:
-    speed = 2
+    speed = 10
     
     def __init__(self, x=0, y=0, filename=None):
         self.x = x
         self.y = y
-        filename = filename or 'images/player.png'
-        self.surface = pygame.image.load(filename)
+        self.image = pygame.image.load(filename or 'images/player.png')
     
     def on_event(self, event):
+        pygame.event.pump()
         keys = pygame.key.get_pressed()
+        if event.type == pygame.KEYDOWN:
+            pass # TODO: shoot bullet
         if keys[pygame.K_w]:
-            self.hero.moveUp()
+            self.moveUp()
         if keys[pygame.K_s]:
-            self.hero.moveDown()
+            self.moveDown()
         if keys[pygame.K_a]:
-            self.hero.moveLeft()
+            self.moveLeft()
         if keys[pygame.K_d]:
-            self.hero.moveRight()
+            self.moveRight()
             
     def moveRight(self):
         self.x += self.speed
